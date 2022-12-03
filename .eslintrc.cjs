62e6737df6595b0f218c8d766c551c32b4a06435
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-12-03 12:01:11 +0100

    chore: setup eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["eslint:recommended", "plugin:react/recommended"],
+	overrides: [],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		"comma-spacing": ["error", { before: false, after: true }],
+		"react/prop-types": "off",
+		// suppress errors for missing 'import React' in files
+		"react/react-in-jsx-scope": "off",
+		// allow jsx syntax in js files (for next.js project)
+		"react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }], //should add ".ts" if typescript project
+	},
+};
