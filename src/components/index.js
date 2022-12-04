bdd24312b0f7e3a8ec49d71293ab241d1fb61ee5
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-12-04 19:52:21 +0100

    feat: components export file

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import SharedLayout from "./SharedLayout/SharedLayout";
+import Home from "./Home/Home";
+import Navbar from "./Navbar/Navbar";
+import Hero from "./Section/Hero/Hero";
+import Footer from "./Section/Footer/Footer";
+
+export { SharedLayout, Home, Navbar, Hero, Footer };
