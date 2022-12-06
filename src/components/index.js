2fa80cf5bab23d9f14eddf0fe4e55c9fef8ca44e
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-12-06 16:48:00 +0100

    export loader and content UI

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import SharedLayout from "./SharedLayout/SharedLayout";
+import Loader from "./Loader/Loader";
 import Home from "./Home/Home";
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Section/Hero/Hero";
+import Content from "./Section/Content/Content";
 import Footer from "./Section/Footer/Footer";
 
-export { SharedLayout, Home, Navbar, Hero, Footer };
+export { SharedLayout, Loader, Home, Navbar, Hero, Content, Footer };
